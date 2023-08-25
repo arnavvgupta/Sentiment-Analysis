08e4cf1230ad581600f38a81a29430f79837a91e
Karanjot Singh <66475280+karanjot-s@users.noreply.github.com>
2023-08-25 17:35:42 +0530

    formated code

--- frontend/src/components/Aboutpage.js
+++ frontend/src/components/Aboutpage.js
@@ -1,36 +1,52 @@
-import React from 'react'
+import React from "react";
 import { useNavigate } from "react-router-dom";
-import Carousel from './Carousel';
-import styles from './Aboutpage.module.css'
+import Carousel from "./Carousel";
+import styles from "./Aboutpage.module.css";
 const Aboutpage = () => {
-    const navigate=useNavigate();
+  const navigate = useNavigate();
   return (
     <div>
-        <div className={styles.mainAbout}>
-            <div className={styles.floating}>
-                <Carousel/>
-                <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti nesciunt labore odio corporis error eaque eligendi, modi harum voluptatibus eveniet.</h3>
-            </div>
-
-            <div className={styles.floating}>
-                <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti nesciunt labore odio corporis error eaque eligendi, modi harum voluptatibus eveniet.</h3>
-                <Carousel/>
-            </div>
-
-            <div className={styles.floating}>
-                <Carousel/>
-                <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti nesciunt labore odio corporis error eaque eligendi, modi harum voluptatibus eveniet.</h3>
-            </div>
-
-            <div className={styles.floating}> 
-                <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti nesciunt labore odio corporis error eaque eligendi, modi harum voluptatibus eveniet.</h3>
-                <Carousel/>
-            </div>
+      <div className={styles.mainAbout}>
+        <div className={styles.floating}>
+          <Carousel />
+          <h3>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti
+            nesciunt labore odio corporis error eaque eligendi, modi harum
+            voluptatibus eveniet.
+          </h3>
         </div>
-        
-        <button onClick={()=>navigate('/urlAnalyser')}>Try Product</button>
-    </div>
-  )
-}
 
-export default Aboutpage
+        <div className={styles.floating}>
+          <h3>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti
+            nesciunt labore odio corporis error eaque eligendi, modi harum
+            voluptatibus eveniet.
+          </h3>
+          <Carousel />
+        </div>
+
+        <div className={styles.floating}>
+          <Carousel />
+          <h3>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti
+            nesciunt labore odio corporis error eaque eligendi, modi harum
+            voluptatibus eveniet.
+          </h3>
+        </div>
+
+        <div className={styles.floating}>
+          <h3>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti
+            nesciunt labore odio corporis error eaque eligendi, modi harum
+            voluptatibus eveniet.
+          </h3>
+          <Carousel />
+        </div>
+      </div>
+
+      <button onClick={() => navigate("/urlAnalyser")}>Try Product</button>
+    </div>
+  );
+};
+
+export default Aboutpage;
--- frontend/src/components/Carousel.js
+++ frontend/src/components/Carousel.js
@@ -1,31 +1,63 @@
-import React from 'react'
-import styles from './Carousel.module.css'
+import React from "react";
+import styles from "./Carousel.module.css";
 const Carousel = () => {
   return (
     <div>
-        <div id="carouselExampleFade" className={`carousel slide carousel-fade ${styles.carouselDiv}`} data-bs-ride="carousel">
+      <div
+        id="carouselExampleFade"
+        className={`carousel slide carousel-fade ${styles.carouselDiv}`}
+        data-bs-ride="carousel"
+      >
         <div className="carousel-inner">
-            <div className="carousel-item active">
-            <img src="https://nationaltoday.com/wp-content/uploads/2022/04/37-MrBeast.jpg" className={styles.ytbImages} alt="..."/>
-            </div>
-            <div className="carousel-item">
-            <img src="https://yt3.googleusercontent.com/lkH37D712tiyphnu0Id0D5MwwQ7IRuwgQLVD05iMXlDWO-kDHut3uI4MgIEAQ9StK0qOST7fiA=s900-c-k-c0x00ffffff-no-rj" className="d-block w-100" alt="..."/>
-            </div>
-            <div className="carousel-item">
-            <img src="https://yt3.googleusercontent.com/enyLBm1Sy8mVRXJJLWHT2z64nqxJGt2g61A9xnxpUjO2YAUovHaY_JT3rnAg0j6Qij9iaHQlAg=s900-c-k-c0x00ffffff-no-rj" className="d-block w-100" alt="..."/>
-            </div>
+          <div className="carousel-item active">
+            <img
+              src="https://nationaltoday.com/wp-content/uploads/2022/04/37-MrBeast.jpg"
+              className={styles.ytbImages}
+              alt="..."
+            />
+          </div>
+          <div className="carousel-item">
+            <img
+              src="https://yt3.googleusercontent.com/lkH37D712tiyphnu0Id0D5MwwQ7IRuwgQLVD05iMXlDWO-kDHut3uI4MgIEAQ9StK0qOST7fiA=s900-c-k-c0x00ffffff-no-rj"
+              className="d-block w-100"
+              alt="..."
+            />
+          </div>
+          <div className="carousel-item">
+            <img
+              src="https://yt3.googleusercontent.com/enyLBm1Sy8mVRXJJLWHT2z64nqxJGt2g61A9xnxpUjO2YAUovHaY_JT3rnAg0j6Qij9iaHQlAg=s900-c-k-c0x00ffffff-no-rj"
+              className="d-block w-100"
+              alt="..."
+            />
+          </div>
         </div>
-        <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
-            <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span className="visually-hidden">Previous</span>
+        <button
+          className="carousel-control-prev"
+          type="button"
+          data-bs-target="#carouselExampleFade"
+          data-bs-slide="prev"
+        >
+          <span
+            className="carousel-control-prev-icon"
+            aria-hidden="true"
+          ></span>
+          <span className="visually-hidden">Previous</span>
         </button>
-        <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
-            <span className="carousel-control-next-icon" aria-hidden="true"></span>
-            <span className="visually-hidden">Next</span>
+        <button
+          className="carousel-control-next"
+          type="button"
+          data-bs-target="#carouselExampleFade"
+          data-bs-slide="next"
+        >
+          <span
+            className="carousel-control-next-icon"
+            aria-hidden="true"
+          ></span>
+          <span className="visually-hidden">Next</span>
         </button>
-        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Carousel
+export default Carousel;
